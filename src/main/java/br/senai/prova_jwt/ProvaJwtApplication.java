fec91b3750f0b39f66e9433978d3fa43e0c155ca
Vinicius de Jesus <viniciussandradej@gmail.com>
2025-09-11 22:02:47 -0300

    Adicionada linha em branco

--- src/main/java/br/senai/prova_jwt/ProvaJwtApplication.java
+++ src/main/java/br/senai/prova_jwt/ProvaJwtApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProvaJwtApplication {
 
+    
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaJwtApplication.class, args);
 	}
