511e00446ca61f769987ac3b1f4e918e34936e82
RochaGR <gutop.rocha@gmail.com>
2025-09-15 15:13:13 -0300

    Adicionado JpaSpecificationExecutor no repository Cargo e Funcionario

--- src/main/java/br/senai/prova_jwt/repository/CargoRepository.java
+++ src/main/java/br/senai/prova_jwt/repository/CargoRepository.java
@@ -2,9 +2,9 @@ package br.senai.prova_jwt.repository;
 
 import br.senai.prova_jwt.model.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CargoRepository extends JpaRepository<Cargo, Long>  {
-
+public interface CargoRepository extends JpaRepository<Cargo, Long>, JpaSpecificationExecutor<Cargo> {
 }
